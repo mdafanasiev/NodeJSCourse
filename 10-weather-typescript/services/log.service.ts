e1ad1a7e3a0f8635fbcbe295bb0d0d021e88d280
mdafanasiev <maxaf278@gmail.com>
2025-10-02 20:31:41 +0300

    Добавил типизацию для приложения пронозы погоды CLI

---
+++ 10-weather-typescript/services/log.service.ts
@@ -0,0 +1,50 @@
+import chalk from "chalk";
+import dedent from "dedent-js";
+
+interface Weather {
+    name: string;
+    weather: {
+        description: string;
+    }[];
+    main: {
+        temp: number;
+        feels_like: number;
+        humidity: number;
+    };
+    wind: {
+        speed: number;
+    };
+}
+
+const printError = (error: string) => {
+    console.log(chalk.bgRed(" ERROR ") + " " + error);
+};
+
+const printSuccess = (message: string) => {
+    console.log(chalk.bgGreen(" SUCCESS ") + " " + message);
+};
+
+const printHelp = () => {
+    console.log(
+        dedent`${chalk.bgCyan(" HELP ")}
+		Без параметров - вывод погоды
+		-s [CITY] для установки городов
+		-h для вывода помощи
+		-t [API_KEY] для сохранения токена
+		-l [LANGUAGE] для установки языка
+		`
+    );
+};
+
+const printWeather = (res: Weather, icon: string) => {
+    console.log(
+        dedent`${chalk.bgYellow(" WEATHER ")} Погода в городе ${res.name}
+		${icon}  ${res.weather[0]!.description}
+		Температура: ${res.main.temp} (ощущается как ${res.main.feels_like})
+		Влажность: ${res.main.humidity}%
+		Скорость ветра: ${res.wind.speed}
+		`
+    );
+};
+
+export { printError, printSuccess, printHelp, printWeather };
